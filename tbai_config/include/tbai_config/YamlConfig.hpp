4d045ca02b67138db1c12abe0f627d4a3758154b
lnotspotl <lnotspotl@gmail.com>
2024-04-12 20:11:50 +0200

    Add two new ROS packages: tbai_docs and tbai_config

--- tbai_config/include/tbai_config/YamlConfig.hpp
+++ tbai_config/include/tbai_config/YamlConfig.hpp
@@ -1,13 +1,34 @@
 #ifndef TBAI_CONFIG_INCLUDE_TBAI_CONFIG_YAMLCONFIG_HPP_
 #define TBAI_CONFIG_INCLUDE_TBAI_CONFIG_YAMLCONFIG_HPP_
 
+#include <yaml-cpp/yaml.h>
+
 #include <string>
 
 class YamlConfig {
    public:
-    YamlConfig() = default;
+    /**
+     * @brief Construct a new Yaml Config object
+     *
+     * @param configPath : Path to yaml config file
+     * @param delim  : config path delimiter
+     */
+    YamlConfig(const std::string &configPath, const std::string &delim = "/");
+
+    /**
+     * @brief Construct YamlConfig with config path specified by a ROS parameter
+     *
+     * @param pathParam : ROS parameter name containing config's path
+     * @param delim : config path delimiter
+     * @return YamlConfig object
+     */
+    static YamlConfig fromRosParam(const std::string &pathParam, const std::string &delim = "/");
 
    private:
+    std::string configPath_;
+    std::string delim_;
 };
+
+#include "tbai_config/implementation/YamlConfig.hpp"
 
 #endif  // TBAI_CONFIG_INCLUDE_TBAI_CONFIG_YAMLCONFIG_HPP_
