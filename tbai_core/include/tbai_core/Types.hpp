6f316c64e95c3ba8cdf0f16bcdc2ad151cd3ccbb
lnotspotl <lnotspotl@gmail.com>
2024-04-12 22:49:19 +0200

    Add tbai_core package

---
+++ tbai_core/include/tbai_core/Types.hpp
@@ -0,0 +1,19 @@
+#ifndef TBAI_CORE_INCLUDE_TBAI_CORE_TYPES_HPP_
+#define TBAI_CORE_INCLUDE_TBAI_CORE_TYPES_HPP_
+
+#include <ocs2_core/Types.h>
+
+namespace tbai {
+
+/** Scalar type */
+using scalar_t = ocs2::scalar_t;
+
+/** Vector type */
+using vector_t = ocs2::vector_t;
+
+/** Matrix type */
+using matrix_t = ocs2::matrix_t;
+
+}  // namespace tbai
+
+#endif  // TBAI_CORE_INCLUDE_TBAI_CORE_TYPES_HPP_
