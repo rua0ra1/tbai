a09e073a18278d42d27fbd9c408e8e9a72e10ec1
lnotspotl <lnotspotl@gmail.com>
2024-04-12 23:11:03 +0200

    Drop ocs2_core dependency for now

--- tbai_core/include/tbai_core/Types.hpp
+++ tbai_core/include/tbai_core/Types.hpp
@@ -1,18 +1,18 @@
 #ifndef TBAI_CORE_INCLUDE_TBAI_CORE_TYPES_HPP_
 #define TBAI_CORE_INCLUDE_TBAI_CORE_TYPES_HPP_
 
-#include <ocs2_core/Types.h>
+#include <Eigen/Dense>
 
 namespace tbai {
 
 /** Scalar type */
-using scalar_t = ocs2::scalar_t;
+using scalar_t = double;
 
 /** Vector type */
-using vector_t = ocs2::vector_t;
+using vector_t = Eigen::Matrix<scalar_t, Eigen::Dynamic, 1>;
 
 /** Matrix type */
-using matrix_t = ocs2::matrix_t;
+using matrix_t = Eigen::Matrix<scalar_t, Eigen::Dynamic, Eigen::Dynamic>;
 
 }  // namespace tbai
 
